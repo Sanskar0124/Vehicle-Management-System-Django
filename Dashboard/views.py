94fc0e1885c876bfcee1bcf083b9a9989a103844
Sanskar <sansakhraliya@gmail.com>
2021-11-10 15:07:10 +0530

    Initial Commiy

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,22 +1,96 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from Driver.models import DriverDoc
 from Travels.models import Travel
-from Vehicle.models import Vehicle
+from Vehicle.models import Vehicle, VehicleMaintainance
 from Vehicle.models import Driver
-import json
 from django.http import JsonResponse
+from datetime import date
 
 # Create your views here.
 
+current_date = date.today()
+
 def master_dashboard(request):
+    # Cards Count
     active = 'Active'
     travels = Travel.objects.filter(status=active)
     vehicle = Vehicle.objects.filter(status=active)
     driver = Driver.objects.filter(status=active)
-    print(travels.count())
-    print(vehicle)
-    print(driver)
-    return json(
-        {
-        'travel_count': '45',
-        })
+    # print(travels.count())
+    # print(vehicle.count())
+    # print(driver.count())
+
+    # Arrays For Counting
+    insuranceExpiry = []
+    pucExpiry = []
+    needServicing = []
+    licenseExpiry = []
+
+    # Vehicle Maintainance
+    vehicleMaintainance = VehicleMaintainance.objects.all()
+    for i in vehicleMaintainance:
+        # Insurance Expiry
+        insDate = i.insurance_exp
+        insRemainingDays =  insDate - current_date
+        # print(remainingDays)
+        strDate = str(insRemainingDays).split()
+        intDate = int(strDate[0])
+        if(intDate < 30):
+            insuranceExpiry.append(insDate)
+            print("red")
+        elif(intDate < 60):
+            insuranceExpiry.append(insDate)
+            print("orange")
+    # print(len(insuranceExpiry))
+
+    # Puc Expiry
+    for i in vehicleMaintainance:
+        pucDate = i.puc_exp
+        pucRemainingDays =  pucDate - current_date
+        # print(pucRemainingDays)
+        strDate = str(pucRemainingDays).split()
+        intDate = int(strDate[0])
+        if(intDate < 30):
+            pucExpiry.append(pucDate)
+            print("pucRed")
+        elif(intDate < 60):
+            pucExpiry.append(pucDate)
+            print("pucOrange")
+
+    # Servicing
+    for i in vehicleMaintainance:
+        serviceDate = i.last_service
+        serviceRemainingDays = serviceDate  - current_date
+        # print(serviceRemainingDays)
+        strDate = str(serviceRemainingDays).split()
+        intDate = int(strDate[0])
+        if(intDate < 90):
+            needServicing.append(serviceDate)
+            print("Need Servicing")
+
+    # License Expiry
+    driverDocs = DriverDoc.objects.all()
+    for i in driverDocs:
+        licDate = i.license_exp_date
+        licRemainingDays =  licDate - current_date
+        # print(pucRemainingDays)
+        strDate = str(licRemainingDays).split()
+        intDate = int(strDate[0])
+        if(intDate < 30):
+            licenseExpiry.append(licDate)
+            print("licRed")
+        elif(intDate < 60):
+            licenseExpiry.append(licDate)
+            print("licOrange")
+            
+    # print(len(pucExpiry))
+    return JsonResponse({'data': {
+        "active_travels" : travels.count(),
+        "active_vehicles" : vehicle.count(),
+        "active_drivers" : driver.count(),
+        "insurance_exp" : len(insuranceExpiry),
+        "puc_exp" : len(pucExpiry),
+        "need_servicing" : len(needServicing),
+        "license_exp" : len(licenseExpiry),
+    }})
--- Vehicle/admin.py
+++ Vehicle/admin.py
@@ -6,6 +6,9 @@ from .models import VehicleDoc,Vehicle, VehicleMaintainance
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ("model", "ownerShip","rc_number","capacity", "vehicle_type", "fuel_type", "permit_type", "status")
 
+class VehicleMaintainanceAdmin(admin.ModelAdmin):
+    list_display = ("last_service", "insurance_exp","insurance_status","puc_exp", "puc_status", "updated_at")
+
 admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(VehicleDoc)
-admin.site.register(VehicleMaintainance)
+admin.site.register(VehicleMaintainance, VehicleMaintainanceAdmin)
